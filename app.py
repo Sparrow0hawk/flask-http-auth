b2d8d7892351304913fcf037daa9862aed121045
Sparrow0hawk <alexjcoleman@gmail.com>
2023-09-15 18:36:03 +0100

    add logic for basic http auth

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
-from flask import Flask
+import functools
+
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
+def check_auth(username, password):
+    return username == "foo" and password == "magic"
+
+
+def login_required(f):
+    @functools.wraps(f)
+    def decorated_func(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return (
+                "Unauthorized",
+                401,
+                {"WWW-Authenticate": "Basic realm='Login Required'"},
+            )
+        return f(*args, **kwargs)
+
+    return decorated_func
+
+
 @app.route("/")
+@login_required
 def index() -> str:
     return "<h1>Hello World!</h1>"
